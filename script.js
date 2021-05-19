6af4551dc90e8f92b623e8faf8d06b41d54cf8dd
mocakbarmaulana <brindel.development@gmail.com>
2021-05-19 12:53:02 +0700

    initial setup modal confirmation

---
+++ script.js
@@ -0,0 +1,9 @@
+const modalConfirmation = document.getElementById("modal-confirmation");
+
+const btnModal = () => {
+  modalConfirmation.style.display = "flex";
+};
+
+const btnCloseModal = () => {
+  modalConfirmation.style.display = "none";
+};
